3f8ee02f7a85db53bb311ac1a430d8df9b8a1c04
Claudio <claudio.junior233@gmail.com>
2022-02-04 20:49:24 -0300

    added info

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,3 +1,14 @@
 export default function About() {
-  return <div>About the project</div>;
+  return (
+    <>
+      <div>Sobre a Api</div>
+      <p>Esse projeto serve como documentação para uma APi de AOT que criei</p>
+      <p>
+        O endpoint para fazer requisições dos personagens é :
+        https://aot-api.herokuapp.com/characters
+      </p>
+
+      <p>No momento, a unica requisição possivel </p>
+    </>
+  );
 }
